f1514c342346837ac4ba62b592df063e20ac40ae
ose0221 <ose0221@naver.com>
2025-01-16 18:37:36 +0900

    ✨ [FEAT]  #12 ENUM 수정

--- src/main/java/sopt/appjam/withsuhyeon/constant/Age.java
+++ src/main/java/sopt/appjam/withsuhyeon/constant/Age.java
@@ -7,11 +7,12 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum Age {
-    EARLY20("20 ~ 24"),
-    LATE20("25 ~ 29"),
-    EARLY30("30 ~ 34"),
-    LATE30("35 ~ 39"),
-    FORTY("40세 이상");
+    EARLY20("EARLY_20", "20 ~ 24"),
+    LATE20("LATE_20", "25 ~ 29"),
+    EARLY30("EARLY_30", "30 ~ 34"),
+    LATE30("LATE_30", "35 ~ 39"),
+    FORTY("FORTY_PLUS", "40세 이상");
 
+    private final String key;
     private final String value;
 }
--- src/main/java/sopt/appjam/withsuhyeon/constant/ProfileImage.java
+++ src/main/java/sopt/appjam/withsuhyeon/constant/ProfileImage.java
@@ -7,9 +7,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ProfileImage {
-    IMAGE1("image1"),
-    IMAGE2("image2"),
-    IMAGE3("image3");
+    IMAGE1("IMAGE_1", "image1"),
+    IMAGE2("IMAGE_2", "image2"),
+    IMAGE3("IMAGE_3", "image3");
 
+    private final String key;
     private final String value;
 }
