e3d9a7f8725ba654b52e6c29a309e4ab20eb1e1c
ose0221 <ose0221@naver.com>
2025-01-20 04:30:46 +0900

    :sparkles: [FEAT] 홈 화면 구현

--- src/main/java/sopt/appjam/withsuhyeon/repository/PostRepository.java
+++ src/main/java/sopt/appjam/withsuhyeon/repository/PostRepository.java
@@ -1,6 +1,7 @@
 package sopt.appjam.withsuhyeon.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import sopt.appjam.withsuhyeon.constant.Region;
 import sopt.appjam.withsuhyeon.domain.PostEntity;
 import sopt.appjam.withsuhyeon.domain.UserEntity;
@@ -9,4 +10,9 @@ import java.util.List;
 
 public interface PostRepository extends JpaRepository<PostEntity, Long> {
     List<PostEntity> findAllByRegion(Region region);
+
+    @Query("SELECT p.id FROM PostEntity p WHERE p.region = :region")
+    List<Long> findIdsByRegion(Region region);
+
+    List<PostEntity> findByIdIn(List<Long> ids);
 }
