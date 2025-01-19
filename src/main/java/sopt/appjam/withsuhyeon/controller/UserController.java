a01583b579ad39b369f6341ee96f2f6be699971b
ose0221 <ose0221@naver.com>
2025-01-19 23:34:34 +0900

    :sparkles: [FEAT] 마이페이지 조회 API

--- src/main/java/sopt/appjam/withsuhyeon/controller/UserController.java
+++ src/main/java/sopt/appjam/withsuhyeon/controller/UserController.java
@@ -1,9 +1,25 @@
 package sopt.appjam.withsuhyeon.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sopt.appjam.withsuhyeon.anotation.UserId;
+import sopt.appjam.withsuhyeon.dto.user.res.UserInfoResponse;
+import sopt.appjam.withsuhyeon.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/v1/mypage")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("")
+    public ResponseEntity<UserInfoResponse> getMyPage(
+            @UserId long userId
+    ) {
+        UserInfoResponse userInfoResponse = userService.getUserInfo(userId);
+        return ResponseEntity.ok(userInfoResponse);
+    }
 }
