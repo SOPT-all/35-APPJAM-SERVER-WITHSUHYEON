bbe9443ce8108b2fa4042fcc2d5f40e873dcfce8
Jang99u <alswkdrb@dgu.ac.kr>
2025-01-21 19:30:05 +0900

    ✨ [FEAT] 유저 id 조회 API 구현

--- src/main/java/sopt/appjam/withsuhyeon/controller/UserController.java
+++ src/main/java/sopt/appjam/withsuhyeon/controller/UserController.java
@@ -6,20 +6,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import sopt.appjam.withsuhyeon.anotation.UserId;
+import sopt.appjam.withsuhyeon.dto.user.res.UserIdResponse;
 import sopt.appjam.withsuhyeon.dto.user.res.UserInfoResponse;
 import sopt.appjam.withsuhyeon.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/mypage")
+@RequestMapping("/api/v1")
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("")
+    @GetMapping("/mypage")
     public ResponseEntity<UserInfoResponse> getMyPage(
             @UserId long userId
     ) {
         UserInfoResponse userInfoResponse = userService.getUserInfo(userId);
         return ResponseEntity.ok(userInfoResponse);
+    }
+
+    @GetMapping("/user-id")
+    public ResponseEntity<UserIdResponse> getUserId(
+            @UserId long userId
+    ) {
+        return ResponseEntity.ok(new UserIdResponse(userId));
     }
 }
---
+++ src/main/java/sopt/appjam/withsuhyeon/dto/user/res/UserIdResponse.java
@@ -0,0 +1,6 @@
+package sopt.appjam.withsuhyeon.dto.user.res;
+
+public record UserIdResponse(
+        Long userId
+) {
+}
