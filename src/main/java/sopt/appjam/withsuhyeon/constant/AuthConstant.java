8ccfbb4f8a02d0070ed0793063362ea6011f71bb
Jang99u <alswkdrb@dgu.ac.kr>
2025-01-22 02:20:43 +0900

    🚨 [HOTFIX] AuthConstant WHITELIST 추가

--- src/main/java/sopt/appjam/withsuhyeon/constant/AuthConstant.java
+++ src/main/java/sopt/appjam/withsuhyeon/constant/AuthConstant.java
@@ -8,7 +8,9 @@ public class AuthConstant {
     public static final String[] AUTH_WHITELIST = {
             "/api/v1/auth/signup",
             "/api/v1/auth/signin",
-            "/chat/**" // WebSocket 경로 추가
+            "/chat/**",
+            "/api/v1/enums/regions",
+            "/api/v1/enums/categories" // WebSocket 경로 추가
     };
     private AuthConstant() {
     }
