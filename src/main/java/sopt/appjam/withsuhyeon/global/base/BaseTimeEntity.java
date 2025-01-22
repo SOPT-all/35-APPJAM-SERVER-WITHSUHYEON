794f2758505db2e74a25f63f148f280250f8bc4f
Jang99u <alswkdrb@dgu.ac.kr>
2025-01-22 23:48:17 +0900

    ✨ [FEAT] #82 게시글 및 갤러리 작성 시 한국 표준시간으로 저장

--- src/main/java/sopt/appjam/withsuhyeon/global/base/BaseTimeEntity.java
+++ src/main/java/sopt/appjam/withsuhyeon/global/base/BaseTimeEntity.java
@@ -1,5 +1,6 @@
 package sopt.appjam.withsuhyeon.global.base;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
@@ -16,9 +17,11 @@ import java.time.LocalDateTime;
 public class BaseTimeEntity {
     @Column(name = "created_date", updatable = false)
     @CreatedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createdDate;
 
     @Column(name = "modified_date")
     @LastModifiedDate
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime modifiedDate;
 }
