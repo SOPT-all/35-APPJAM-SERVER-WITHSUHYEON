7e07bf42cec26d0e2b06c3ea7883102393c42e5b
ose0221 <ose0221@naver.com>
2025-01-23 00:01:55 +0900

    :recycle: [REFAC] #84 ErrorCode 리팩터링

--- src/main/java/sopt/appjam/withsuhyeon/exception/ConstantErrorCode.java
+++ src/main/java/sopt/appjam/withsuhyeon/exception/ConstantErrorCode.java
@@ -9,9 +9,9 @@ import sopt.appjam.withsuhyeon.global.exception.ErrorCode;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ConstantErrorCode implements ErrorCode {
-    NOT_FOUND_CATEGORY(HttpStatus.BAD_REQUEST, "CATEGORY_001","유효하지 않은 카테고리 입니다."),
-    NOT_FOUND_PROFILE_IMAGE(HttpStatus.BAD_REQUEST, "CATEGORY_002","유효하지 않은 프로필 이미지 입니다."),
-    NOT_FOUND_LOCATION_FROM_SUBLOCATION(HttpStatus.BAD_REQUEST, "CATEGORY_003","해당 하위지역에 해당하는 상위지역을 찾을 수 없습니다.");
+    NOT_FOUND_CATEGORY(HttpStatus.BAD_REQUEST, "CONSTANT_001","유효하지 않은 카테고리 입니다."),
+    NOT_FOUND_PROFILE_IMAGE(HttpStatus.BAD_REQUEST, "CONSTANT_002","유효하지 않은 프로필 이미지 입니다."),
+    NOT_FOUND_LOCATION_FROM_SUBLOCATION(HttpStatus.BAD_REQUEST, "CONSTANT_003","해당 하위지역에 해당하는 상위지역을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String errorCode;
--- src/main/java/sopt/appjam/withsuhyeon/exception/PostErrorCode.java
+++ src/main/java/sopt/appjam/withsuhyeon/exception/PostErrorCode.java
@@ -9,7 +9,7 @@ import sopt.appjam.withsuhyeon.global.exception.ErrorCode;
 @Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum PostErrorCode implements ErrorCode {
-    POST_PRICE_INVALID_INPUT(HttpStatus.BAD_REQUEST, "POST_001", "사례금은 ? ~ 99999원 범위 내에서 입력 가능합니다."),
+    POST_PRICE_INVALID_INPUT(HttpStatus.BAD_REQUEST, "POST_001", "사례금은 0 ~ 99999원 범위 내에서 입력 가능합니다."),
     POST_AGE_INVALID_INPUT(HttpStatus.BAD_REQUEST, "POST_002", "유효하지 않는 나이대를 입력하였습니다."),
     POST_REGION_INVALID_INPUT(HttpStatus.BAD_REQUEST, "POST_003", "유효하지 않은 관심 지역을 입력하였습니다."),
     POST_REQUEST_INVALID_INPUT(HttpStatus.BAD_REQUEST, "POST_004", "유효하지 않은 요청 사항을 입력하였습니다."),
