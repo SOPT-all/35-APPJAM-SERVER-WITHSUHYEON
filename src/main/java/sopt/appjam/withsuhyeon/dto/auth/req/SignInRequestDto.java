f8e30f8f54a1c0ba47d923a411d458ce36ee5bf2
ose0221 <ose0221@naver.com>
2025-01-23 18:32:32 +0900

    :recycle: [REFAC] #92 Controller에 전화번호 정규 표현식 @Valid 추가

--- src/main/java/sopt/appjam/withsuhyeon/dto/auth/req/SignInRequestDto.java
+++ src/main/java/sopt/appjam/withsuhyeon/dto/auth/req/SignInRequestDto.java
@@ -1,6 +1,10 @@
 package sopt.appjam.withsuhyeon.dto.auth.req;
 
+import jakarta.validation.constraints.Pattern;
+
 public record SignInRequestDto(
+        @Pattern(regexp = "^01([0|1|6|7|8|9])\\d{4}\\d{4}", message = "로그인할 번호는 전화번호 형식에 맞게 11자로 입력해야 합니다.")
+
         String phoneNumber
 ) {
 }
